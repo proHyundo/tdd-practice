a47b79619ae97e7ae8d690ea9faa7a358daa56ba
proHyundo <guseh08@knou.ac.kr>
2025-07-11 02:14:21 +0900

    feat: 포인트 사용 동시성 이슈 처리를 위한 테스트 추가

--- src/test/java/io/hhplus/tdd/point/service/PointConcurrencyProblemTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointConcurrencyProblemTest.java
@@ -58,4 +58,39 @@ public class PointConcurrencyProblemTest {
         UserPoint userPoint = pointService.getUserPointByUserId(1L);
         assertThat(userPoint.point()).isEqualTo(1000 * threadCount);
     }
+
+    @DisplayName("포인트 사용 API는, 동시 사용 요청이 있을 때에도 올바르게 처리되어야 한다")
+    @Test
+    void useConcurrencyTest() {
+        int threadCount = 50;
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        // 초기 포인트 설정
+        userPointTable.insertOrUpdate(1L, 5000);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    // 사용 요청을 보내는 로직
+                    pointService.useUserPoint(1L, 100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            log.info("InterruptedException occurred: {}", e.getMessage());
+        }
+
+        // then
+        UserPoint userPoint = pointService.getUserPointByUserId(1L);
+        assertThat(userPoint.point()).isEqualTo(5000 - (100 * threadCount));
+    }
 }
